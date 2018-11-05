c0187637609e80562fb6ffc8d4ec2208f813ba50
jwidic <jwidic@gmail.com>
2018-11-05 17:41:58 +0800

    change the code framework add MP4encoder.c and .h file

---
+++ MP4Encoder.h
@@ -0,0 +1,80 @@
+/********************************************************************
+filename:   MP4Encoder.h
+created:    2013-04-16
+author:     firehood
+purpose:    MP4编码器，基于开源库mp4v2实现（https://code.google.com/p/mp4v2/）。
+*********************************************************************/
+#include "include/mp4v2/mp4v2.h"
+const static char HEAD[75]={0x00,0x00,0x00,0x01,0x27,0x64,0x00,0x28,
+                            0xad,0x84,0x05,0x45,0x62,0xb8,0xac,0x54,
+                            0x71,0x08,0x0a,0x8a,0xc5,0x71,0x58,0xa8,
+                            0xe2,0x10,0x24,0x85,0x21,0x39,0x3c,0x9f,
+                            0x27,0xe4,0xfe,0x4f,0xc9,0xf2,0x79,0xb9,
+                            0xb3,0x4d,0x08,0x12,0x42,0x90,0x9c,0x9e,
+                            0x4f,0x93,0xf2,0x7f,0x27,0xe4,0xf9,0x3c,
+                            0xdc,0xd9,0xa6,0x17,0x2a,0x01,0x40,0x16,
+                            0xe4,0x00,0x00,0x00,0x01,0x28,0xfe,0x01,
+                            0xae,0x2c};
+const static char NAL[5]={0x00,0x00,0x00,0x01}; //h264 frame head
+// NALU单元
+typedef struct _MP4ENC_NaluUnit
+{
+	int type;
+	int size;
+	unsigned char *data;
+}MP4ENC_NaluUnit;
+
+typedef struct _MP4ENC_Metadata
+{
+	// video, must be h264 type
+	unsigned int    nSpsLen;
+	unsigned char   Sps[1024];
+	unsigned int    nPpsLen;
+	unsigned char   Pps[1024];
+
+} MP4ENC_Metadata, *LPMP4ENC_Metadata;
+
+struct H264_MP4ENCODER
+{
+	int m_nWidth;
+	int m_nHeight;
+	int m_nFrameRate;
+	int m_nTimeScale;
+	MP4TrackId m_videoId;
+	MP4FileHandle hMp4file;
+
+}F_H264_MP4ENCODER;
+
+struct H264_MP4DECODER
+{
+    unsigned int nSize;
+    unsigned char *pData;
+    MP4Timestamp pStartTime;
+    MP4Duration  pDuration;
+    MP4Duration  pRenderingOffset;
+    unsigned int VnumSamples;
+	MP4TrackId m_videoId;
+	MP4FileHandle hMp4file;
+
+}F_H264_MP4DECODER;
+
+    //init basic struct variable
+    void MP4Encoder_init(void);
+	// open or creat a mp4 file.
+	bool CreateMP4File(const char *fileName, int width, int height, int timeScale, int frameRate);
+	// wirte 264 metadata in mp4 file.
+	bool Write264Metadata(MP4FileHandle hMp4File, LPMP4ENC_Metadata lpMetadata);
+	// wirte 264 data, data can contain  multiple frame.
+	int WriteH264Data(MP4FileHandle hMp4File, const unsigned char* pData, int size);
+	// close mp4 file.
+	void CloseMP4File(MP4FileHandle hMp4File);
+	// convert H264 file to mp4 file.
+	// no need to call CreateMP4File and CloseMP4File,it will create/close mp4 file automaticly.
+	bool WriteH264File(const char* pFile264, const char* pFileMp4);
+	// Prase H264 metamata from H264 data frame
+	bool PraseMetadata(const unsigned char* pData, int size, MP4ENC_Metadata *metadata);
+	// read one nalu from H264 data buffer
+	int ReadOneNaluFromBuf(const unsigned char *buffer, unsigned int nBufferSize, unsigned int offSet, MP4ENC_NaluUnit *nalu);
+
+    int closemp4file( MP4FileHandle *oMp4File );
+    int openmp4file(char *sMp4file,MP4FileHandle *oMp4File);
--- main.c
+++ main.c
@@ -6,24 +6,13 @@
 #include <time.h> 
 #include <sys/time.h>
 #include "include/mp4v2/mp4v2.h"
+#include "MP4Encoder.h"
 
 
+//#define CBB_REC_FILENAME                        "/mnt/cbb/cbbRec.h264"
 
 int get264stream(MP4FileHandle oMp4File,int VTrackId,int totalFrame,char *name)
 {
-
-    const static char HEAD[75]={0x00,0x00,0x00,0x01,0x27,0x64,0x00,0x28,
-                            0xad,0x84,0x05,0x45,0x62,0xb8,0xac,0x54,
-                            0x71,0x08,0x0a,0x8a,0xc5,0x71,0x58,0xa8,
-                            0xe2,0x10,0x24,0x85,0x21,0x39,0x3c,0x9f,
-                            0x27,0xe4,0xfe,0x4f,0xc9,0xf2,0x79,0xb9,
-                            0xb3,0x4d,0x08,0x12,0x42,0x90,0x9c,0x9e,
-                            0x4f,0x93,0xf2,0x7f,0x27,0xe4,0xf9,0x3c,
-                            0xdc,0xd9,0xa6,0x17,0x2a,0x01,0x40,0x16,
-                            0xe4,0x00,0x00,0x00,0x01,0x28,0xfe,0x01,
-                            0xae,0x2c};
-
-    const static char NAL[5]={0x00,0x00,0x00,0x01}; //h264 frame head
     unsigned char *pData=NULL;
     unsigned char *p =NULL;
     unsigned int i=0;
@@ -34,7 +23,7 @@ int get264stream(MP4FileHandle oMp4File,int VTrackId,int totalFrame,char *name)
     MP4Duration  pRenderingOffset;
     unsigned long framesize=0x00000000;
     bool pIsSyncSample =0;
-    int nReadIndex =0;
+    static int nReadIndex =0;
     FILE *pFile = NULL;
 
     static bool FirstIdx = FALSE;
@@ -60,6 +49,7 @@ int get264stream(MP4FileHandle oMp4File,int VTrackId,int totalFrame,char *name)
 //            printf("nSize=%d\n",nSize);
             if( FirstIdx == TRUE ){
                 printf("%d\n",nSize+74);
+                FirstIdx = FALSE;
             }else{
                 printf("%d\n",nSize);
             }
@@ -83,69 +73,23 @@ int get264stream(MP4FileHandle oMp4File,int VTrackId,int totalFrame,char *name)
     return 0;
 }
 
-int openmp4file(char *sMp4file,char *Vname,char *Aname,MP4FileHandle *oMp4File)
-{
-    int i;
 
-    //unsigned int oStreamDuration;
-    unsigned int VFrameCount;
-    unsigned int AFrameCount;
 
-    *oMp4File = MP4Read(sMp4file);
-    int videoindex = -1,audioindex = -1;
-    uint32_t VnumSamples;
-    uint32_t AnumSamples;
-    //uint32_t timescale;
-    //uint64_t duration;        
 
-    if (!*oMp4File)
-    {
-        printf("Read error....%s\r\n",sMp4file);
-        return -1;
+
+
+
+int main(int argc,char **argv){
+
+    openmp4file("cbbRec.mp4", &F_H264_MP4DECODER.hMp4file);
+
+
+    if(F_H264_MP4DECODER.m_videoId >= 0){
+       get264stream(F_H264_MP4DECODER.hMp4file,F_H264_MP4DECODER.m_videoId,F_H264_MP4DECODER.VnumSamples,"test.h264");
+ // int fget264stream(MP4FileHandle oMp4File,int VTrackId,int totalFrame)
     }
 
-    MP4TrackId trackId = MP4_INVALID_TRACK_ID;
-    uint32_t numTracks = MP4GetNumberOfTracks(*oMp4File,NULL,0);//获取音视频轨道数 2 
-    //  printf("numTracks:%d\n",numTracks);
-
-    for (i = 0; i < numTracks; i++)
-    {
-        trackId = MP4FindTrackId(*oMp4File, i,NULL,0); //获取trackId，判断获取数据类型 1 获取视频数据，2 获取音频数据
-//        printf("trackId:%d\n",trackId);
-        const char* trackType = MP4GetTrackType(*oMp4File, trackId);
-        if (MP4_IS_VIDEO_TRACK_TYPE(trackType)){
-        //printf("[%s %d] trackId:%d\r\n",__FUNCTION__,__LINE__,trackId);
-        videoindex= trackId;
-
-         //duration = MP4GetTrackDuration(oMp4File, trackId );
-        VnumSamples = MP4GetTrackNumberOfSamples(*oMp4File, trackId);
-        //timescale = MP4GetTrackTimeScale(oMp4File, trackId);
-        //oStreamDuration = duration/(timescale/1000);
-        VFrameCount = VnumSamples;
-
-        }else if (MP4_IS_AUDIO_TRACK_TYPE(trackType)){
-            audioindex = trackId;
-            AnumSamples = MP4GetTrackNumberOfSamples(*oMp4File, trackId);
-            AFrameCount = AnumSamples;
-            //  printf("audioindex:%d\n",audioindex);
-        }
-    }
-    //解析完了mp4，主要是为了获取sps pps 还有video的trackID
-    if(videoindex >= 0){
-        get264stream(*oMp4File,videoindex,VFrameCount,Vname);
-    }else if(audioindex >= 0){
-    //GetAacStream(oMp4File,aduindex,AFrameCount,Aname);
-    }
-
-    //需要mp4close 否则在嵌入式设备打开mp4上多了会内存泄露挂掉.
-    MP4Close(*oMp4File,0);
-    return 0;
-}
-
-int main(int argc,char **argv)
-{
-    MP4FileHandle oMp4File;
-    openmp4file("cbbRec.mp4", "test.h264","test.26", &oMp4File);
+    closemp4file(&F_H264_MP4DECODER.hMp4file);
     return 0;
 }
 
